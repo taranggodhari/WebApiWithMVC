d4d7331d6eff0c10f8afb83ce105be1f1c0ddfa6
Dark_Wave\Tarang Godhari <taranggodhari@gmail.com>
2017-08-29 17:17:55 -0400

    Add project files.

---
+++ WebApiWithMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApiWithMVC.Startup))]
+namespace WebApiWithMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
